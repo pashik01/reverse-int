3b166c6453979f98239396a533e1a0744028fe86
pashik01 <105845661+pashik01@users.noreply.github.com>
2022-07-14 14:38:19 +0300

    feat: add is file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) {
+    n =-n;
+  }
+
+  let strN = n.toString();
+  let resultRev = '';
+  for (let i=0; i<strN.length; i++) {
+    if ((strN[i]=== 0) && (i === strN.length - 1)) {
+        resultRev = resultRev;
+    } else {
+        resultRev = `${strN[i]}${resultRev}`;
+    }
+  }
+
+  return +resultRev;
 }
